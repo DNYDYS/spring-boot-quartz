9037588455b6846a59432a40f6065e4f45fd34a2
DNYDYS <1786087581@qq.com>
2021-12-12 00:36:57 +0800

    springboot集成quartz

---
+++ src/main/java/com/dnydys/task/quartz/SpringBootDemoTaskQuartzApplication.java
@@ -0,0 +1,22 @@
+package com.dnydys.task.quartz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import tk.mybatis.spring.annotation.MapperScan;
+
+/**
+ * <p>
+ * 启动器
+ * </p>
+ *
+ * @author dnydys
+ * @date Created in 2021-12-11 23:48
+ */
+@MapperScan(basePackages = {"com.dnydys.task.quartz.mapper"})
+@SpringBootApplication
+public class SpringBootDemoTaskQuartzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDemoTaskQuartzApplication.class, args);
+    }
+}
---
+++ src/main/java/com/dnydys/task/quartz/mapper/JobMapper.java
@@ -0,0 +1,24 @@
+package com.dnydys.task.quartz.mapper;
+
+import com.dnydys.task.quartz.entity.domain.JobAndTrigger;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+/**
+ * <p>
+ * Job Mapper
+ * </p>
+ *
+ * @author dnydys
+ * @date Created in 2021-12-11 23:48
+ */
+@Component
+public interface JobMapper {
+    /**
+     * 查询定时作业和触发器列表
+     *
+     * @return 定时作业和触发器列表
+     */
+    List<JobAndTrigger> list();
+}
